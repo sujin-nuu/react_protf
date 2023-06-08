93fab59f26024e7c5c29f7596295d194da906c7e
김수진 <yhs04378@gmail.com>
2023-06-08 17:00:20 +0900

    framer-motion add , main layout setting

--- src/common/layout/main/MainLayout.tsx
+++ src/common/layout/main/MainLayout.tsx
@@ -1,9 +1,70 @@
+import { useState } from "react";
 import { Outlet } from "react-router-dom"
+import 'src/assets/css/main.css';
+import cloudIcon from 'src/assets/img/cloud.png';
 
 export const MainLayout = () => {
+
+    const [xy, setXY] = useState({ x: 0, y: 0 })
+
+    const xyHandler = (e: any) => {
+        const mouseX = e.clientX;
+        const mouseY = e.clientY;
+
+        setXY({ x: mouseX, y: mouseY });
+    }
+
     return (
-        <div style={{width:'100vw', height: '100vh'}}>
-            <Outlet />
+        <div
+            style={{ width: '100vw', height: '100vh', overflowX: 'hidden' }}
+            onMouseMove={xyHandler}
+        >
+            <div
+                /* 마우스 아이콘 */
+                className='pointer'
+                style={{
+                    transform: `translate(${xy.x}px, ${xy.y}px)`
+                }}
+            />
+            <div
+                /* 탑 메뉴 */
+                style={{ display: 'flex', justifyContent: 'space-between', marginTop: '1rem', overflowX: 'hidden' }}>
+                <div className="nav" style={{ width: '8%', marginLeft: '2rem' }}>
+                    <img src={cloudIcon} width={60} height={60} alt="mainIcon" />
+                </div>
+                <div className="nav" style={{ width: '80%', marginTop: '1.5rem' }}>
+                    <span className="menuItem">
+                        Career
+                    </span>
+                    <span className="menuItem">
+                        Portfolio
+                    </span>
+                    <span className="menuItem">
+                        contact
+                    </span>
+                </div>
+                <div style={{ width: '10%' }}>
+
+                </div>
+            </div>
+            <div
+                className="content">
+                <div style={{ width: '10%' }} >
+                    <div 
+                        className="noDrag"
+                        style={{ textCombineUpright: 'none', writingMode: 'vertical-lr', textOrientation: 'upright', margin: '1rem', position: 'fixed' }}>
+                        <span>HELLO</span>
+                        <span
+                            className="cloudEmoj">☁</span>
+                        <span>こんにちは</span>
+                        <span style={{ textCombineUpright: 'all', marginTop: 10, marginBottom: 20 }}>: )</span>
+                    </div>
+                </div>
+                <div style={{ width: '80%' }}>
+                    <Outlet />
+                </div>
+                <div style={{ width: '10%' }} />
+            </div>
         </div>
     )
 }
--- src/features/mainDash/pages/MainDash.tsx
+++ src/features/mainDash/pages/MainDash.tsx
@@ -1,31 +1,17 @@
-import cloudImg from 'src/assets/img/clouds-bg5.jpg';
-import 'src/assets/css/main.css';
-import { useState } from 'react';
-
 const MainDash = () => {
-
-    const [xy, setXY] = useState({ x: 0, y: 0 })
-
-    const xyHandler = (e: any) => {
-        const mouseX = e.clientX;
-        const mouseY = e.clientY;
-
-        setXY({ x: mouseX, y: mouseY });
-
-    }
 
     //#afd6ff
     return (
         <div
-            onMouseMove={xyHandler}
             style={{
                 width: '100%',
                 height: '100%',
                 backgroundColor: '#FFFFFF'
             }}>
-            <div className='pointer' style={{
-                transform: `translate(${xy.x}px, ${xy.y}px)`
-            }} />
+            
+            <div style={{width: '100%'}}>
+                <h2>1111</h2>
+            </div>
         </div>
     )
 }
