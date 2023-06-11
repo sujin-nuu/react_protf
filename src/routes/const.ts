520d857f6e29de086589b47c60d61f50dcfe9e2f
김수진 <yhs04378@gmail.com>
2023-06-12 08:40:45 +0900

    router change

--- src/routes/const.ts
+++ src/routes/const.ts
@@ -1,10 +1,10 @@
-export const ROUTE_ROOT = '';
+export const ROUTE_ROOT = '/react_protf';
 
 /** 메인 */
-export const ROUTE_MAIN = `/`;
+export const ROUTE_MAIN = `${ROUTE_ROOT}/`;
 
-export const ROUTE_CAREER ='/career';
+export const ROUTE_CAREER =`${ROUTE_ROOT}/career`;
 
-export const ROUTE_PORTFOLIO ='/portf';
+export const ROUTE_PORTFOLIO =`${ROUTE_ROOT}/portf`;
 
-export const ROUTE_CONTACT ='/contact';
+export const ROUTE_CONTACT =`${ROUTE_ROOT}/contact`;
